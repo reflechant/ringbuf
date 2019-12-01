cdecf06db046d6a3ce91d2687c58ce42472425a7
R.G <reflechant@users.noreply.github.com>
2019-12-01 23:47:35 +0300

    Added some basic functions

---
+++ ringbuf.c
@@ -0,0 +1,53 @@
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct ringbuf
+{
+    size_t cap;
+    uint8_t *base;
+    uint8_t *head;
+    uint8_t *tail;
+};
+
+bool is_empty(struct ringbuf *buf)
+{
+    return buf->tail == buf->head;
+}
+
+size_t nfilled(struct ringbuf *buf)
+{
+    if (buf->head >= buf->tail)
+    {
+        return buf->head - buf->tail;
+    }
+    else
+    {
+        return buf->cap - (buf->tail - buf->head);
+    }
+}
+
+size_t nfree(struct ringbuf *buf)
+{
+    return buf->cap - nfilled(buf);
+}
+
+size_t headtoend(struct ringbuf *buf)
+{
+    return buf->cap - (buf->head - buf->base);
+}
+
+size_t tailtoend(struct ringbuf *buf)
+{
+    return buf->cap - (buf->tail - buf->base);
+}
+
+uint8_t *next_head(struct ringbuf *buf, size_t offset)
+{
+    return buf->base + ((buf->head - buf->base) + offset) % buf->cap;
+}
+
+uint8_t *next_tail(struct ringbuf *buf, size_t offset)
+{
+    return buf->base + ((buf->tail - buf->base) + offset) % buf->cap;
+}
