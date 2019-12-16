5b04eb0666e90211c032a03b22089e6e5ff2e0c7
R.G <reflechant@users.noreply.github.com>
2019-12-16 18:02:24 +0300

    added alternative memcpy functions using SIMD

--- ringbuf.c
+++ ringbuf.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <x86intrin.h>
 
 struct ringbuf
 {
@@ -10,6 +11,61 @@ struct ringbuf
     uint8_t *head;
     uint8_t *tail;
 };
+
+// Alternative realization of memcpy using SIMD instructions. Looks like 256-bit version is the fastest even if AVX-512 is available.
+// It's a naive realization which works best for small chunks (up to ~1500 byte - maximum size of standard IEEE 802.3 frame payload). For bigger chunks search for `apex_kryptonite` which uses prefetch and other tricks
+void *intrin_memcpy_512(void *restrict dst, const void *restrict src, size_t size)
+{
+    size_t i = 0;
+    for (; i < size; i += 64)
+    {
+        const __m512i c = _mm512_loadu_si512(src + i);
+        _mm512_storeu_si512(dst + i, c);
+    }
+    intrin_memcpy_256(dst + i, src + i, size - i);
+
+    return dst;
+}
+
+void *intrin_memcpy_256(void *restrict dst, const void *restrict src, size_t size)
+{
+    size_t i = 0;
+    for (; i < size; i += 32)
+    {
+        const __m256i c = _mm256_loadu_si256((__m256i_u *)(src + i));
+        _mm256_storeu_si256((__m256i_u *)(dst + i), c);
+    }
+    intrin_memcpy_128(dst + i, src + i, size - i);
+
+    return dst;
+}
+
+void *intrin_memcpy_128(void *restrict dst, const void *restrict src, size_t size)
+{
+    size_t i = 0;
+    for (; i < size; i += 16)
+    {
+        const __m128i c = _mm_loadu_si128((__m128i *)(src + i));
+        _mm_storeu_si128((__m128i *)(dst + i), c);
+    }
+    for (; i < size; i += 8)
+    {
+        const uint64_t c = *((uint64_t *)(src + i));
+        *((uint64_t *)(dst + i)) = c;
+    }
+    for (; i < size; i += 4)
+    {
+        const uint32_t c = *((uint32_t *)(src + i));
+        *((uint32_t *)(dst + i)) = c;
+    }
+    for (; i < size; i++)
+    {
+        const uint8_t c = *((uint8_t *)(src + i));
+        *((uint8_t *)(dst + i)) = c;
+    }
+
+    return dst;
+}
 
 bool is_empty(struct ringbuf *buf)
 {
